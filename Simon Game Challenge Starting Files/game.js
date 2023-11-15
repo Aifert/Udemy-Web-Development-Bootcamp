060dec9c63381cb1063d1f119ed5cd34ab190413
Aifert Yet <aifertyet0628@gmail.com>
2023-11-15 13:08:04 +0800

    Simon game

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,82 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var gameStarted = false;
+var level = 0;
+
+$(document).keydown(function(){
+    if(!gameStarted){
+        $("#level-title").text("Level "+ level);
+        nextSequence();
+        gameStarted = true;
+    }
+})
+
+$(".btn").click(function(){
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+
+})
+
+
+
+function nextSequence(){
+    userClickedPattern = [];
+    level ++;
+
+    $("#level-title").text("Level "+ level);
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else{
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        playSound("wrong");
+        $("body").addClass("game-over");
+
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+}
