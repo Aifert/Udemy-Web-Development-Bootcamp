cead5a3b6aaad2cfa5dc4aa979ae6f62b271e707
Aifert Yet <aifertyet0628@gmail.com>
2023-11-19 18:52:19 +0800

    axios

--- Backend/5.2 JSON/index.js
+++ Backend/5.2 JSON/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 //Step 1: Run the solution.js file without looking at the code.
 //Step 2: You can go to the recipe.json file to see the full structure of the recipeJSON below.
@@ -12,11 +12,30 @@ const recipeJSON =
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var recipe;
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {Recipe : recipe});
 });
 
 app.post("/recipe", (req, res) => {
+  const choice = req.body["choice"];
+
+  switch (choice){
+    case "chicken":
+      recipe = JSON.parse(recipeJSON)[0];
+      break;
+    case "beef":
+      recipe = JSON.parse(recipeJSON)[1];
+      break;
+    case "fish":
+      recipe = JSON.parse(recipeJSON)[2];
+      break;
+    default : 
+      recipe = "invalid prompt";
+      break;
+  }
+  res.redirect("/");
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
 });
