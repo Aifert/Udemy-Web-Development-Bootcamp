ce895063757e747aac6eacbbf3588062a6e4a8d7
Aifert Yet <aifertyet0628@gmail.com>
2023-11-24 22:48:09 +0800

    started authentication

---
+++ Secrets - Starting Code/app.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended : true}));
+
+mongoose.connect("mongodb://loalhost:27017/userDB", {useNewUrlParser : true});
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.listen(port, () => {
+    console.log("Server successfully started on port 3000");
+})
